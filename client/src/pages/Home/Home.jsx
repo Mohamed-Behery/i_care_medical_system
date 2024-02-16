820d19728273f0ddb682afb7777a7f04ebda1a44
Mohamed Behery <MohamedBehery.Coding@Gmail.Com>
2024-02-17 01:11:55 +0200

    Designed the services section and made it responsive

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import styles from "./Home.module.css";
 import doctorImg from "./../../images/hero-section-img.png";
 import doctorsImg from "./../../images/about-section-img.png";
+import doctorCard from "./../../images/doctor-services.png";
+import pharmacyCard from "./../../images/pharmacy-services.png";
+import labCard from "./../../images/lab-services.png";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -47,6 +50,44 @@ const Home = () => {
               Our focus is on innovation, precision, and excellence in
               delivering comprehensive solutions for clinics,
               labs, and pharmacies.
+            </p>
+          </div>
+        </div>
+      </section>
+      {/*  SERVICES SECTION  */}
+      <section className={styles.services} id="services">
+        <h2>Our Services</h2>
+        <div className={styles.cards}>
+          <div className={styles.card}>
+            <img src={doctorCard} alt="Doctor Services" />
+            <h3>Doctor</h3>
+            <p>
+              A medical facility providing healthcare services to patients.
+              Clinics serve as spaces for disease diagnosis, treatment, and
+              initial examinations. They encompass a variety of medical
+              specialties to meet diverse patient needs.
+            </p>
+          </div>
+          <div className={styles.card}>
+            <img src={pharmacyCard} alt="Pharmacy Services" />
+            <h3>Pharmacy</h3>
+            <p>
+              A facility that dispenses medications and medical supplies.
+              Pharmacists offer guidance on medication usage, determine
+              appropriate dosages, and ensure the safety of medications.
+              Pharmacies play a vital role in patient care, facilitating access
+              to essential treatments.
+            </p>
+          </div>
+          <div className={styles.card}>
+            <img src={labCard} alt="Lab Services" />
+            <h3>Laboratory</h3>
+            <p>
+              A medical laboratory is a facility where blood tests, urine
+              analyses, and other diagnostic procedures are conducted.
+              Laboratories aim to provide accurate results for disease diagnosis
+              and ongoing monitoring of patients' health. Labs are considered
+              cornerstone in healthcare delivery.
             </p>
           </div>
         </div>
