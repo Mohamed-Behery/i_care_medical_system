c89c6f6a7d79aa069a24ba149bcfb31e5124c4bc
Mohamed Behery <MohamedBehery.Coding@Gmail.Com>
2024-02-27 03:08:05 +0200

    Recreated the Header and made it responsive

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -7,6 +7,7 @@ import {
   faArrowRight,
   faBars,
   faAngleDown,
+  faTimes,
 } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Header.module.css";
 import HomeIcon from "./../../images/home-icon.svg";
@@ -14,7 +15,7 @@ import ReservationsIcon from "./../../images/reservations-icon.svg";
 import ChatIcon from "./../../images/chat-icon.svg";
 import ProfileImg from "./../../images/profile.svg";
 
-const Header = () => {
+const Nav = () => {
   const [menuOpen, setMenuOpen] = useState(true);
   const [profileMenu, setprofileMenu] = useState(false);
   const location = useLocation();
@@ -29,7 +30,7 @@ const Header = () => {
 
   return (
     <header>
-      <div className={styles.left}>
+      <div className={styles.navBar}>
         <Link className={styles.logoWrapper} to="/home">
           <img
             className={[styles.logo, styles.logoIcon].join(" ")}
@@ -42,109 +43,121 @@ const Header = () => {
             alt="Logo Text"
           />
         </Link>
-        <nav className={`${!menuOpen ? styles.open : ""}`}>
-          <ul>
-            {location.pathname !== "/reservations" &&
-              location.pathname !== "/chat" &&
-              location.pathname !== "/profile" && (
-                <>
-                  <li>
-                    <a className={styles.link} href="/home">
-                      Home
-                    </a>
-                  </li>
-                  <li>
-                    <a className={styles.link} href="/home#about">
-                      About US
-                    </a>
-                  </li>
-                  <li>
-                    <a className={styles.link} href="/home#services">
-                      Services
-                    </a>
-                  </li>
-                  <li>
-                    <a className={styles.link} href="/home#faq">
-                      FAQ
-                    </a>
-                  </li>
-                  <li>
-                    <a className={styles.link} href="/home#contact">
-                      Contact
-                    </a>
-                  </li>
-                </>
-              )}
-            {(location.pathname === "/reservations" ||
-              location.pathname === "/chat" ||
-              location.pathname === "/profile") && (
+
+        <div className={styles.navToggle} onClick={menuToggle}>
+          <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
+        </div>
+
+        <ul className={`${styles.navList} ${menuOpen ? styles.active : ""}`}>
+          {location.pathname !== "/reservations" &&
+            location.pathname !== "/chat" &&
+            location.pathname !== "/profile" && (
               <>
-                <li>
+                <li className={styles.navItem}>
                   <a className={styles.link} href="/home">
-                    <img src={HomeIcon} alt="Home" />
+                    Home
                   </a>
                 </li>
-                <li>
-                  <a className={styles.link} href="/reservations">
-                    <img src={ReservationsIcon} alt="Reservations" />
+                <li className={styles.navItem}>
+                  <a className={styles.link} href="/home#about">
+                    About US
                   </a>
                 </li>
-                <li>
-                  <a className={styles.link} href="/chat">
-                    <img src={ChatIcon} alt="Chat" />
+                <li className={styles.navItem}>
+                  <a className={styles.link} href="/home#services">
+                    Services
+                  </a>
+                </li>
+                <li className={styles.navItem}>
+                  <a className={styles.link} href="/home#faq">
+                    FAQ
+                  </a>
+                </li>
+                <li className={styles.navItem}>
+                  <a className={styles.link} href="/home#contact">
+                    Contact
                   </a>
                 </li>
               </>
             )}
-          </ul>
-        </nav>
-      </div>
-      <FontAwesomeIcon
-        className={styles.menu}
-        icon={faBars}
-        onClick={menuToggle}
-      />
-      {(location.pathname === "/home" ||
-        location.pathname === "/" ||
-        !location.pathname) && (
-        <div className={styles.right}>
-          <div>
-            <Link className={[styles.link, styles.login].join(" ")} to="/login">
-              Login
-            </Link>
-            <Link
-              className={[styles.link, styles.signup].join(" ")}
-              to="/signup"
-            >
-              Sign up
-              <FontAwesomeIcon className={styles.arrow} icon={faArrowRight} />
-            </Link>
-          </div>
-        </div>
-      )}
-      {(location.pathname === "/reservations" ||
-        location.pathname === "/profile" ||
-        location.pathname === "/chat") && (
-        <div className={styles.right}>
-          <div className={styles.profileCard} onClick={profileMenuToggle}>
-            <img src={ProfileImg} alt="Profile" />
-            <span>Carter Smith</span>
-            <FontAwesomeIcon icon={faAngleDown} />
-          </div>
-          {profileMenu && (
-            <div className={`${styles.profileMenu} ${styles.profileMenu}`}>
-              <ul>
-                <li>
-                  <Link to={"/profile"} className={styles.link}>Profile</Link>
-                </li>
-                <li><Link to={"/home"} className={styles.link}>Logout</Link></li>
-              </ul>
-            </div>
+          {(location.pathname === "/reservations" ||
+            location.pathname === "/chat" ||
+            location.pathname === "/profile") && (
+            <>
+              <li className={styles.navItem}>
+                <a className={styles.link} href="/home">
+                  <img src={HomeIcon} alt="Home" />
+                </a>
+              </li>
+              <li className={styles.navItem}>
+                <a className={styles.link} href="/reservations">
+                  <img src={ReservationsIcon} alt="Reservations" />
+                </a>
+              </li>
+              <li className={styles.navItem}>
+                <a className={styles.link} href="/chat">
+                  <img src={ChatIcon} alt="Chat" />
+                </a>
+              </li>
+            </>
           )}
-        </div>
-      )}
+          <li className={styles.navItem}>
+            {(location.pathname === "/home" ||
+              location.pathname === "/" ||
+              !location.pathname) && (
+              <div className={styles.rightLinks}>
+                <Link
+                  className={[styles.link, styles.login].join(" ")}
+                  to="/login"
+                >
+                  Login
+                </Link>
+                <Link
+                  className={[styles.link, styles.signup].join(" ")}
+                  to="/signup"
+                >
+                  Sign up
+                  <FontAwesomeIcon
+                    className={styles.arrow}
+                    icon={faArrowRight}
+                  />
+                </Link>
+              </div>
+            )}
+            {(location.pathname === "/reservations" ||
+              location.pathname === "/profile" ||
+              location.pathname === "/chat") && (
+              <>
+                <div className={styles.profileCard} onClick={profileMenuToggle}>
+                  <img src={ProfileImg} alt="Profile" />
+                  <span>Carter Smith</span>
+                  <FontAwesomeIcon icon={faAngleDown} />
+                </div>
+                {profileMenu && (
+                  <div
+                    className={`${styles.profileMenu} ${styles.profileMenu}`}
+                  >
+                    <ul>
+                      <li>
+                        <Link to={"/profile"} className={styles.link}>
+                          Profile
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to={"/home"} className={styles.link}>
+                          Logout
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </>
+            )}
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
 
-export default Header;
+export default Nav;
